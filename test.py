48aa1764e95878096c4f6f6e05b6e777da829991
Dan Heeks <danheeks@gmail.com>
2009-02-06 23:39:50 +0000

    added round_corner_factor to offset. Also, added arcs for input

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 import area
 
-a1 = area.new()
-area.add_point(a1, 0, 10, 10, 0, 0)
-area.add_point(a1, 0, 30, 10, 0, 0)
-area.add_point(a1, 0, 30, 30, 0, 0)
-area.add_point(a1, 0, 10, 30, 0, 0)
-area.add_point(a1, 0, 10, 10, 0, 0)
+a = area.new()
+area.add_point(a, 0, 10, 10, 0, 0)
+area.add_point(a, 0, 30, 10, 0, 0)
+area.add_point(a, 0, 30, 30, 0, 0)
+area.add_point(a, 0, 10, 30, 0, 0)
+area.add_point(a, 0, 10, 10, 0, 0)
 
-area.offset(a1, 3)
-for curve in range(0, area.num_curves(a1)):
-    for vertex in range(0, area.num_vertices(a1, curve)):
-        sp, x, y, cx, cy = area.get_vertex(a1, curve, vertex)
+area.offset(a, 3)
+for curve in range(0, area.num_curves(a)):
+    for vertex in range(0, area.num_vertices(a, curve)):
+        sp, x, y, cx, cy = area.get_vertex(a, curve, vertex)
         print sp, x, y, cx, cy
 
