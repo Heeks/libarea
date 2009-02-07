cea894b86b3ec6f22ab57175277c9297a121afa5
danheeks <danheeks@1755c4d2-f44b-11dd-802b-177316e4ac06>
2009-02-07 00:56:54 +0000

    oops, I wrote January instead of February in copyright

--- trunk/PythonStuff.cpp
+++ trunk/PythonStuff.cpp
@@ -1,6 +1,6 @@
 // PythonStuff.cpp
 
-// written by Dan Heeks, January 6th 2009, license: GPL version 3 http://www.gnu.org/licenses/gpl-3.0.txt
+// written by Dan Heeks, February 6th 2009, license: GPL version 3 http://www.gnu.org/licenses/gpl-3.0.txt
 
 #include "PythonStuff.h"
 
